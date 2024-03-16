4007f59c760fe3e8047e535a407bf27f8faefb9b
VshalTrivedi <121444662+VshalTrivedi@users.noreply.github.com>
2024-03-16 23:59:04 +0530

    First Commit

---
+++ src/test/java/activity/NavigationTest.java
@@ -0,0 +1,35 @@
+package activity;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class NavigationTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+
+        // set path of Chromedriver executable
+        System.setProperty("webdriver.chrome.driver",
+                "./src/test/resources/drivers/chromedriver/chromedriver.exe");
+
+        // initialize new WebDriver session
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void navigateToAUrl() {
+        // navigate to the web site
+        driver.get("http://navihedge-fe.s3-website-ap-southeast-2.amazonaws.com/Client");
+        // Validate page title
+        Assert.assertEquals(driver.getTitle(), "NaviHedge");
+    }
+    @AfterMethod
+    public void afterMethod() {
+
+        // close and quit the browser
+        driver.quit();
+    }
+}
